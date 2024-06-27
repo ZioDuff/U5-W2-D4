262fc847bbc34d249e97862cbb4e00d5da087aa0
Jacopo De maio <jajodemaio@gmail.com>
2024-06-27 14:20:59 +0200

    Initial commit

---
+++ ValidationAndUpload/src/main/java/JacopoDeMaio/ValidationAndUpload/ValidationAndUploadApplication.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.ValidationAndUpload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationAndUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidationAndUploadApplication.class, args);
+	}
+
+}
---
+++ ValidationAndUpload/src/test/java/JacopoDeMaio/ValidationAndUpload/ValidationAndUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package JacopoDeMaio.ValidationAndUpload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationAndUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
